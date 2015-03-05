6a17881931a09bcd9a810a268c22b6563d5b3394
Andrew Karlov <dein.negativ@gmail.com>
2015-03-05 16:05:07 +0300

    * alberto:   * work on async-interface is in progress.

--- src/alberto_async.ml
+++ src/alberto_async.ml
@@ -9,6 +9,20 @@ let (>>@) d f =
   | Some x -> f x
   | None -> raise Scheduler_not_running
 
+let forward x = x
+
+let take_int r =
+  let buf = String.create 4 in
+  Reader.really_read r ~pos:0 ~len:4 buf
+  >>| (function | `Eof _ ->  raise End_of_file
+                | `Ok -> BE.get_int32 buf 0 |> Int32.to_int)
+
+let put_int w i =
+  let hdr =
+    let buf = Bytes.create 4 in
+    let () = BE.set_int32 buf 0 @@ Int32.of_int i in Bytes.to_string buf in
+    return @@ Writer.write w hdr;
+
 module Make(A: (module type of Alberto)) = struct
    type t = A.t Deferred.t
 
@@ -19,52 +33,44 @@ module Make(A: (module type of Alberto)) = struct
      term >>| A.encode
 
    let decode str = 
-     str >>= (fun s -> match A.decode s with
-                       | None -> return None
-                       | Some x -> return (Some (create x)))
+     match A.decode str with
+     | None -> return None
+     | Some x -> return @@ Some (create x)
 
    let encode_exn term =
      term >>| A.encode_exn
 
    let decode_exn str =
-     str >>= (fun str -> create (A.decode_exn str))
+     create (A.decode_exn str)
 
    let to_string term =
      term >>| A.to_string
 
    let read_term r =
-     let len =
-       let buf = String.create 4 in
-       Reader.really_read r ~pos:0 ~len:4 buf
-       >>| (function | `Eof _ -> raise End_of_file
-                     | `Ok -> BE.get_int32 buf 0 
-                              |> Int32.to_int)
-       >>@ (fun len -> len) in
-     let buf = String.create len in
-     Reader.really_read r ~pos:0 ~len:len buf
-     >>= (function | `Eof _ -> raise End_of_file
-                   | `Ok -> decode_exn (return buf))
+     take_int r
+     >>| (fun len -> String.create len)
+     >>= (fun buf -> Reader.really_read r ~pos:0 ~len:(String.length buf) buf
+                     >>= (function | `Eof _ -> raise End_of_file
+                                   | `Ok -> decode_exn buf))
 
    let write_term w term =
      encode_exn term 
      >>| (fun str -> 
-              let hdr = 
-                  let buf = Bytes.create 4 in
-                  let () = BE.set_int32 buf 0 (Int32.of_int (String.length str)) in Bytes.to_string buf in
-              Writer.write w hdr;
+              String.length str |> put_int w >>> forward;
               encode_exn term >>> (fun str -> Writer.write w str);
-              Writer.flushed w >>> fun () -> ())
+              Writer.flushed w >>> forward)
 
+   let interact (f: A.t -> t) =
+     let r = Lazy.force Reader.stdin and
+         w = Lazy.force Writer.stdout in
+     let rec loop () =
+       read_term r 
+       >>= (function | `Atom "stop" -> exit 0
+                     | `Atom "ping" -> create (`Atom "pong")
+                     | term -> f term)
+       >>= (fun x -> write_term w (create x))
+       >>= loop in try loop () with End_of_file -> exit 0
 end
-
-
-
-
-
-
-
-
-
 
 
 
--- src/alberto_async.mli
+++ src/alberto_async.mli
@@ -7,16 +7,21 @@ module Make(A: (module type of Alberto)) : sig
 
   val encode: t -> (string option) Deferred.t
 
-  val decode: string Deferred.t -> (t option) Deferred.t
+  val decode: string -> (t option) Deferred.t
 
   val encode_exn: t -> string Deferred.t
 
-  val decode_exn: string Deferred.t -> t
+  val decode_exn: string -> t
 
   val to_string: t -> string Deferred.t
 
   val read_term: Reader.t -> t
 
   val write_term: Writer.t -> t -> unit Deferred.t
+
+  val interact: (A.t -> t) -> unit Deferred.t
 end
+
+val (>>@): 'a Deferred.t -> ('a -> 'b) -> 'b
+val forward: 'a -> 'a
 
